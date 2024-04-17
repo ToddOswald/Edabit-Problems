3062d8cd44cb72da055d1184654589293dadfe16
ToddOswald <lilswagboi0@gmail.com>
2024-04-17 15:37:57 -0400

    Smaller of 2 Numbers
    Converts string to int to string then pushes out smaller of 2 numbers

---
+++ C#/Smaller of 2 Numbers.cs
@@ -0,0 +1,30 @@
+public static string smallerNum(string n1, string n2)
+{
+    int number1 = int.Parse(n1);
+    int number2 = int.Parse(n2);
+
+    int min = Math.Min(number1,number2);
+
+    string result = min.ToString(); 
+
+    return result;
+}
+    
+
+
+/*
+
+convert string to int
+
+find the bigger number
+
+convert int to string
+
+select the one that was higher as an int
+
+
+return result
+
+
+
+*/
