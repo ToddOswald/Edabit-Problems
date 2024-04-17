1c19663490f87957b0f8242ee1f10db3cb31d660
ToddOswald <lilswagboi0@gmail.com>
2024-04-16 21:40:57 -0400

    Hamming Value
    creates a hamming value for two strings

---
+++ C#/Hamming Value.cs
@@ -0,0 +1,19 @@
+public static int HammingDistance(string str1, string str2)
+{
+
+    int count = 0;
+
+    for (int i = 0; i <str1.Length; i++)
+    {
+        if (str1[i]!= str2[i])
+        {
+            count++;
+        }
+        
+        
+    }
+
+    return count;
+}
+
+// creates a hamming value for two strings
