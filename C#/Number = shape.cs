7c99bf68c677761281ca48f2adb5fce2098dee13
ToddOswald <lilswagboi0@gmail.com>
2024-04-23 13:19:47 -0400

    Create Number = shape.cs
    Use a switch case to output different shapes from numbers 1-10.
    
    Learned when to use a switch case

---
+++ C#/Number = shape.cs
@@ -0,0 +1,31 @@
+public static string NSidedShape(int n)
+{
+
+    
+    switch(n)
+    {
+        case 1:
+            return "circle";
+        case 2:
+            return "semi-circle";
+        case 3:
+            return "triangle";
+        case 4:
+            return "square";
+        case 5: 
+            return "pentagon";
+        case 6: 
+            return "hexagon";
+        case 7:
+            return "heptagon";
+        case 8: 
+            return "octagon";
+        case 9: 
+            return "nonagon";
+        case 10:
+            return "decagon";
+
+    }
+
+    return "error";
+}
