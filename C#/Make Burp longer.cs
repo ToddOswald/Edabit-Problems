06418dfc3fdddb6e307e480602e7414f78f26174
ToddOswald <lilswagboi0@gmail.com>
2024-04-25 12:28:43 -0400

    Create Make Burp longer.cs
    makes burp with different length of r base off given number. learned to use the new string function

---
+++ C#/Make Burp longer.cs
@@ -0,0 +1,13 @@
+        public static string LongBurp(int b) 
+	{
+        
+        string manyR = new string ('r', b);
+
+        return "Bu" + manyR + "p";
+	}
+
+    /*
+
+
+Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+*/
